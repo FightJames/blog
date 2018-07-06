09631d9f9726f00dd322a4fb129114b60ba7b31d
James <as1234ert@gmail.com>
2018-07-06 14:14:32 +0800

    add jobschedule

--- Service/app/src/main/java/com/techapp/james/service/jobSheduler/JobSchedulerActivity.kt
+++ Service/app/src/main/java/com/techapp/james/service/jobSheduler/JobSchedulerActivity.kt
@@ -1,13 +1,46 @@
 package com.techapp.james.service.jobSheduler
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.pm.ComponentInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
 import com.techapp.james.service.R
+import kotlinx.android.synthetic.main.activity_job_scheduler.*
 
 class JobSchedulerActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_job_scheduler)
+        scheduleBtn.setOnClickListener {
+            var componentName = ComponentName(this, JobSchedulerService::class.java)
+            var info = JobInfo.Builder(123, componentName)
+                    .setRequiresCharging(false)
+                    // 是否是充電狀態
+                    //Specify that to run this job, the device must be charging (or be a non-battery-powered device connected to permanent power, such as Android TV devices).
+                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                    // 網路條件，default 是 NETWORK_TYPE_NONE
+                    .setPersisted(true)
+                    //手機重啟後是否繼續執行
+                    .setPeriodic(15 * 60 * 1000)
+                    //設定循環執行的時長
+                    .build()
+            var jobScheduler = this.getSystemService(android.content.Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            var resultCode = jobScheduler.schedule(info)
+            if (resultCode == JobScheduler.RESULT_SUCCESS) {
+                Log.d("Job Activity ", "Job scheduled")
+            } else {
+                Log.d("Job Activity ", "Job failed")
+            }
+        }
+        cancelBtn.setOnClickListener {
+            var jobScheduler = this.getSystemService(android.content.Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            jobScheduler.cancel(123)
+            Log.d("Job Activity ", "Job cancelled")
+        }
     }
 }
--- Service/app/src/main/java/com/techapp/james/service/jobSheduler/JobSchedulerService.kt
+++ Service/app/src/main/java/com/techapp/james/service/jobSheduler/JobSchedulerService.kt
@@ -2,17 +2,45 @@ package com.techapp.james.service.jobSheduler
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.util.Log
 
 class JobSchedulerService : JobService() {
-    override fun onStopJob(params: JobParameters?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    companion object {
+        private var TAG = "MY_JOB_SCHEDULER_SERVICE"
+        private var jobCancelled = false
+    }
 
+    override fun onStopJob(params: JobParameters?): Boolean {
+        Log.d(TAG, " Job cancelled before completion")
+        jobCancelled = true
+        return true
     }
 
     override fun onStartJob(params: JobParameters?): Boolean {
-
+        Log.d(TAG, " Job started")
+        doBackgroundWork(params)
         //return true : It means task will take a lot of time, it needs to call onStopJob to destroy
         //return false : It means task will finish in here(return).
-        return false
+        return true
+    }
+
+    private fun doBackgroundWork(params: JobParameters?) {
+        Thread(object : Runnable {
+            override fun run() {
+                for (i in 0..10) {
+                    Log.d(TAG, " run: " + i)
+                    if (jobCancelled) {
+                        return
+                    }
+                    try {
+                        Thread.sleep(1000)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+                }
+                Log.d(TAG, " Job finished")
+                jobFinished(params, false)
+            }
+        }).start()
     }
 }
