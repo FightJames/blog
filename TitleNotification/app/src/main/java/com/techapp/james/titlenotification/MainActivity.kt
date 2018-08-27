70214f4b4f69eb1dab38d6a8a5d718945851043b
James <as1234ert@gmail.com>
2018-08-27 11:51:47 +0800

    update notification

--- TitleNotification/app/src/main/java/com/techapp/james/titlenotification/MainActivity.kt
+++ TitleNotification/app/src/main/java/com/techapp/james/titlenotification/MainActivity.kt
@@ -4,16 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.support.v4.app.NotificationCompat
-import android.content.Context.NOTIFICATION_SERVICE
 import android.app.NotificationManager
 import android.content.Context
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.Bitmap
-import android.support.v4.content.ContextCompat
-import android.graphics.drawable.Drawable
 import android.app.NotificationChannel
 import android.os.Build
-import android.support.annotation.RequiresApi
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,42 +18,52 @@ class MainActivity : AppCompatActivity() {
 
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        val mBuilder = NotificationCompat.Builder(this)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelLove = NotificationChannel(
+            val channel = NotificationChannel(
                     "Channel ID",
                     "Channel Name",
                     NotificationManager.IMPORTANCE_HIGH)
-            channelLove.description = "Hello People"
-            channelLove.enableLights(true)
-            channelLove.enableVibration(true)
-            mNotificationManager.createNotificationChannel(channelLove)
+
+
+//            Returns the user visible description of this channel.
+            channel.description = "Hello"
+            channel.enableLights(true)
+//            Sets whether notifications posted to this channel should display notification lights, on devices that support that feature.
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(0, 1000, 1000, 1000)
+            mNotificationManager.createNotificationChannel(channel)
+
+            val mBuilder = NotificationCompat.Builder(this, "Channel ID")
 
             mBuilder.setContentTitle("Hello")
             mBuilder.setSmallIcon(R.drawable.ic_launcher_foreground)
             mBuilder.setAutoCancel(true)
             mBuilder.setContentInfo("Info")
             mBuilder.setTicker("狀態列")
-//            mBuilder.setPriority(NotificationCompat.PRIORITY_MAX)
 
-//            mBuilder.setDefaults(NotificationCompat.DEFAULT_SOUND)
-            //設定震動方式，延遲0秒，震動1秒，延遲1秒、震動一秒
-//            mBuilder.setVibrate(longArrayOf(0, 1000, 1000, 1000))
-            mBuilder.setChannelId("Channel ID")
+
+
+            button.setOnClickListener {
+                mNotificationManager.notify(1, mBuilder.build())
+            }
         } else {
+
+            val mBuilder = NotificationCompat.Builder(this)
             mBuilder.setContentTitle("Hello")
             mBuilder.setSmallIcon(R.drawable.ic_launcher_foreground)
             mBuilder.setAutoCancel(true)
             mBuilder.setContentInfo("Info")
+//            Set this flag if you would only like the sound, vibrate and ticker to be played if the notification is not already showing.
             mBuilder.setTicker("狀態列")
             mBuilder.setPriority(NotificationCompat.PRIORITY_MAX)
-
+//mBuilder.setLargeIcon()
             mBuilder.setDefaults(NotificationCompat.DEFAULT_SOUND)
             //設定震動方式，延遲0秒，震動1秒，延遲1秒、震動一秒
             mBuilder.setVibrate(longArrayOf(0, 1000, 1000, 1000))
-        }
-        button.setOnClickListener {
-            mNotificationManager.notify(1, mBuilder.build())
+
+            button.setOnClickListener {
+                mNotificationManager.notify(1, mBuilder.build())
+            }
         }
     }
 }
